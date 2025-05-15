96763dd73999eafbd3c705c76cf8a642019f5289
HanzzXOffc <ptgudanggaram30@gmail.com>
2025-05-16 00:41:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript functionality can be added here if needed
+document.querySelector('.buy-button').addEventListener('click', function() {
+    window.open('https://wa.me/6281936513894', '_blank');
+});
